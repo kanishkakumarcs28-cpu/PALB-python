c515b4401995e4e80eb5e02c105e7fb39ff96fda
Kanishka Kumar <kanishka.kumar.cs28@iilm.edu>
2026-02-11 23:35:30 +0530

    Create Q-15 Merge_Array.py

---
+++ Q-15 Merge_Array.py
@@ -0,0 +1,20 @@
+15.class Solution:
+    def mergeArrays(self, a, b):
+        n = len(a)
+        m = len(b)
+        
+        i = n - 1
+        j = 0
+        
+        # Step 1: Swap elements if needed
+        while i >= 0 and j < m:
+            if a[i] > b[j]:
+                a[i], b[j] = b[j], a[i]
+                i -= 1
+                j += 1
+            else:
+                break
+        
+        # Step 2: Sort both arrays
+        a.sort()
+        b.sort()
