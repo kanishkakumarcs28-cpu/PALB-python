ded72ed2b189e323ca1887a1ec793bf9eaa5ef3f
Kanishka Kumar <kanishka.kumar.cs28@iilm.edu>
2026-02-14 17:56:44 +0530

    Create Q-30 Row with max 1s.py

---
+++ Q-30 Row with max 1s.py
@@ -0,0 +1,13 @@
+30.class Solution:
+    def rowWithMaxOnes(self, arr):
+        n = len(arr)
+        m = len(arr[0])
+        max_row_index = -1
+        j = m - 1
+
+        for i in range(n):
+            while j >= 0 and arr[i][j] == 1:
+                j -= 1
+                max_row_index = i
+
+        return max_row_index
