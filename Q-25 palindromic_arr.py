2e1ab74b539ff676716e3db40a1a11856fe9ba4a
Kanishka Kumar <kanishka.kumar.cs28@iilm.edu>
2026-02-14 16:37:47 +0530

    Create Q-25 palindromic_arr.py

---
+++ Q-25 palindromic_arr.py
@@ -0,0 +1,15 @@
+25.
+def isPalinArray(arr):
+    
+    for num in arr:
+        original = num
+        rev = 0
+        
+        while num > 0:
+            rev = rev * 10 + (num % 10)
+            num //= 10
+        
+        if original != rev:
+            return False
+    
+    return True
