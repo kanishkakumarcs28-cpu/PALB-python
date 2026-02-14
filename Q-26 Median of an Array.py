af73e54d045e52d969edf056ea913856068a535d
Kanishka Kumar <kanishka.kumar.cs28@iilm.edu>
2026-02-14 17:25:07 +0530

    Q-26 Median of an Array.py

--- Q-26 Median of an Array.py
+++ Q-26 Median of an Array.py
@@ -2,13 +2,17 @@
     def findMedian(self, arr):
          arr.sort()
          n = len(arr)
-         if n//2 == 0:
-             mid1 = len(n)//2 - 1
-             mid2 = len(n)//2
+         if n%2 == 0:
+             mid1 = n//2 - 1
+             mid2 = n//2
              return(arr[mid1] + arr[mid2]) / 2
              
          else:
              return arr[n//2]
+        
+            
+        
+            
         
             
         
