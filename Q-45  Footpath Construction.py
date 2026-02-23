4628c30530a3be8fbdd70392aae26831035f3de8
Kanishka Kumar <kanishka.kumar.cs28@iilm.edu>
2026-02-23 17:00:10 +0530

    Q-45  Footpath Construction.py

--- Q-45  Footpath Construction.py
+++ Q-45  Footpath Construction.py
@@ -1,30 +1,59 @@
 45.class Solution{
     int[] createFootpath(int n,int m,int[][] a,int q,int[][] queries){
+        int[][] tl=new int[n][m];
+        int[][] tr=new int[n][m];
+        int[][] bl=new int[n][m];
+        int[][] br=new int[n][m];
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                int val=a[i][j];
+                if(i==0&&j==0) tl[i][j]=val;
+                else if(i==0) tl[i][j]=Math.min(val,tl[i][j-1]);
+                else if(j==0) tl[i][j]=Math.min(val,tl[i-1][j]);
+                else tl[i][j]=Math.min(val,Math.min(tl[i-1][j],tl[i][j-1]));
+            }
+        }
+        
+        for(int i=0;i<n;i++){
+            for(int j=m-1;j>=0;j--){
+                int val=a[i][j];
+                if(i==0&&j==m-1) tr[i][j]=val;
+                else if(i==0) tr[i][j]=Math.min(val,tr[i][j+1]);
+                else if(j==m-1) tr[i][j]=Math.min(val,tr[i-1][j]);
+                else tr[i][j]=Math.min(val,Math.min(tr[i-1][j],tr[i][j+1]));
+            }
+        }
+        
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<m;j++){
+                int val=a[i][j];
+                if(i==n-1&&j==0) bl[i][j]=val;
+                else if(i==n-1) bl[i][j]=Math.min(val,bl[i][j-1]);
+                else if(j==0) bl[i][j]=Math.min(val,bl[i+1][j]);
+                else bl[i][j]=Math.min(val,Math.min(bl[i+1][j],bl[i][j-1]));
+            }
+        }
+        
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                int val=a[i][j];
+                if(i==n-1&&j==m-1) br[i][j]=val;
+                else if(i==n-1) br[i][j]=Math.min(val,br[i][j+1]);
+                else if(j==m-1) br[i][j]=Math.min(val,br[i+1][j]);
+                else br[i][j]=Math.min(val,Math.min(br[i+1][j],br[i][j+1]));
+            }
+        }
+        
         int[] ans=new int[q];
         for(int k=0;k<q;k++){
             int R=queries[k][0]-1;
             int C=queries[k][1]-1;
             int sum=0;
-            int min1=Integer.MAX_VALUE;
-            for(int i=0;i<R;i++)
-                for(int j=0;j<C;j++)
-                    min1=Math.min(min1,a[i][j]);
-            if(min1!=Integer.MAX_VALUE) sum+=min1;
-            int min2=Integer.MAX_VALUE;
-            for(int i=0;i<R;i++)
-                for(int j=C+1;j<m;j++)
-                    min2=Math.min(min2,a[i][j]);
-            if(min2!=Integer.MAX_VALUE) sum+=min2;
-            int min3=Integer.MAX_VALUE;
-            for(int i=R+1;i<n;i++)
-                for(int j=0;j<C;j++)
-                    min3=Math.min(min3,a[i][j]);
-            if(min3!=Integer.MAX_VALUE) sum+=min3;
-            int min4=Integer.MAX_VALUE;
-            for(int i=R+1;i<n;i++)
-                for(int j=C+1;j<m;j++)
-                    min4=Math.min(min4,a[i][j]);
-            if(min4!=Integer.MAX_VALUE) sum+=min4;
+            if(R>0&&C>0) sum+=tl[R-1][C-1];
+            if(R>0&&C<m-1) sum+=tr[R-1][C+1];
+            if(R<n-1&&C>0) sum+=bl[R+1][C-1];
+            if(R<n-1&&C<m-1) sum+=br[R+1][C+1];
             ans[k]=sum;
         }
         return ans;
